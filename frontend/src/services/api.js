7fa33ccdf8f8e2f61af83e266c65131a84755d22
Nusrath Fathima <nusrathfathima564@gmail.com>
2026-05-15 20:49:03 +0530

    fix backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: https://mini-crm-1-fc32.onrender.com/api/leads"
+  baseURL: "https://mini-crm-1-fc32.onrender.com/api"
 });
 
 export default API;
